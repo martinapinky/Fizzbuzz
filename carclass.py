d8420ecd24dd5cdbeec7d9506915194cb2cbd1fd
Martina Alinda <martinaalinda1@gmail.com>
2017-04-18 16:05:52 +0300

    Changes

--- carclass.py
+++ carclass.py
@@ -3,21 +3,32 @@ class Car(object):
     def __init__(self):
         self.model = 'GM'
         self.name = "General"
+        self.number_of_doors = 4
 
     def __init__(self, name, model):
         self.model = model
         self.name = name
+        if self.name == 'Porshe' or 'Koenigsegg':
+            self.number_of_doors = 2
+        else:
+            self.number_of_doors = 4
 
     def __init__(self, name):
         self.name = name
         self.model = 'GM'
-
-    def number_of_doors(self):
         if self.name == 'Porshe' or 'Koenigsegg':
-            return 2
+            self.number_of_doors = 2
         else:
-            return 4
-    def
+            self.number_of_doors = 4
+
+
+    def __init__(self, name, model, ):
+        self.model = model
+        self.name = name
+        if self.name == 'Porshe' or 'Koenigsegg':
+            self.number_of_doors = 2
+        else:
+            self.number_of_doors = 4
 
 
 
